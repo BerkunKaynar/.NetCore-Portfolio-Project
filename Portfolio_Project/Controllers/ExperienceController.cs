81b25fad96964dbdca78e34bbfa57a923661bc34
Berkun Kaynar <berkun-kaynar@hotmail.com>
2024-04-15 17:24:25 +0300

    Experience admin

---
+++ Portfolio_Project/Controllers/ExperienceController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Project.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceDal());
+        public IActionResult Index()
+        {
+            var values = experienceManager.TGetList();
+            return View(values);
+        }
+    }
+}
